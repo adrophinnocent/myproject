eaf5800f87103de8a0290857dbdb8a4a07e42a98
adrophinnocent <adrophinnocent@gmail.com>
2026-07-30 16:46:13 +0300

    SEO: Fix schema yield in layout

--- resources/views/public/layouts/app.blade.php
+++ resources/views/public/layouts/app.blade.php
@@ -98,6 +98,9 @@
     {{-- Custom Header Scripts --}}
     {!! \App\Models\Setting::get('header_scripts') !!}
 
+    {{-- SEO Schema --}}
+    @yield('schema')
+
     {{-- Fonts & CSS --}}
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,700;1,400&family=Inter:wght@300;400;500;600;700&family=Cormorant+Garamond:ital,wght@0,400;0,600;0,700;1,400&display=swap" rel="stylesheet" media="print" onload="this.media='all'">
     <noscript>
